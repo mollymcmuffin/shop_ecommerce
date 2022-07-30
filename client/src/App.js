22f3eaeda82a4c4fe3971b08e2beeb934619a7dd
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-07-30 12:42:34 +0800

    added dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,38 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 function App() {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+
+        <Route path="/products" element={<ProductList />}>
+          <Route path=":category" element={<ProductList />} />
+        </Route>
+
+        <Route path="/product" element={<Product />}>
+          <Route path=":id" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/login" element={user ? <Home /> : <Login />}></Route>
+        <Route
+          path="/register"
+          element={user ? <Home /> : <Register />}
+        ></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
